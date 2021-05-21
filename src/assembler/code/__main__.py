fd5eedb3f872c4cf72d280d9634bc3d6dcfef70c
A00x40 <Ahmedattia621@yahoo.com>
2021-05-21 21:21:52 +0200

    Added Constants & Fixed Pylance not recognizing local modules

--- src/assembler/code/__main__.py
+++ src/assembler/code/__main__.py
@@ -2,9 +2,21 @@ import sys
 import click
 
 from io_lib import IO
+import constants
 
 def main():
     pass
+
+def wordize(lines):
+    """
+    Parse and tokenize lines
+    """
+    for l in lines :
+        print(l)
+
+    words = []
+    return words
+    
 # Main Command
 @main.command()
 @click.argument('in_file', required=True)
@@ -17,7 +29,7 @@ def compile(**kwargs):
     lines = io_man.readFileIntoLines(kwargs.get("in_file"))
 
     # Translate to binary codes
-    out_lines = lines
+    out_lines = wordize(lines)
 
     # Write to file
     io_man.writeLineToFile(kwargs.get("out_file"), out_lines)
@@ -29,6 +41,6 @@ def compile(**kwargs):
 
 if __name__ == "__main__.py" :
     args = sys.argv
-    if len(args) == 1:
+    if len(args) == 3:
         print("Pipelined Assembler")
     main()
